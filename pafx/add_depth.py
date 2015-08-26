b2565021604ae4185f2d2b44168dd769740f75fb
Aurélien Gâteau <aurelien@trinity>
2015-08-26 14:24:39 +0200

    doc++

--- pafx/add_depth.py
+++ pafx/add_depth.py
@@ -2,6 +2,7 @@ from pafx.utils import get_color_value, istransparent, process_pixels
 
 
 def add_depth(src, color='black'):
+    """Add a 1px line below the non-transparent pixels of src"""
     def fcn(dst, src, xy):
         if not istransparent(src, xy):
             return
--- pafx/add_outline.py
+++ pafx/add_outline.py
@@ -2,6 +2,7 @@ from pafx.utils import get_color_value, istransparent, process_pixels
 
 
 def add_outline(src, color='black'):
+    """Add a 1px outline around the non-transparent pixels of src"""
     def fcn(dst, src, xy):
         if not istransparent(src, xy):
             return
